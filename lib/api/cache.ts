3859763e07e893aa122b89a236fc6466768841c2
a5-001 <orvill.delatorre@atom5engineering.com>
2026-03-22 18:51:35 -0400

    -

--- lib/api/cache.ts
+++ lib/api/cache.ts
@@ -9,7 +9,7 @@ interface CacheEntry<T> {
 }
 
 class APICache {
-  private cache: Map<string, CacheEntry<any>> = new Map();
+  private cache: Map<string, CacheEntry<unknown>> = new Map();
   private ttl: number = 5 * 60 * 1000; // 5 minutes default TTL
 
   /**
