4911d128dbfd3cb2206596626f8da89f020bf201
a5-001 <orvill.delatorre@atom5engineering.com>
2026-04-21 01:36:22 -0400

    -

--- components/pipelines/pipeline-selector.tsx
+++ components/pipelines/pipeline-selector.tsx
@@ -25,7 +25,7 @@ interface PipelineSelectorProps {
   currentAdjusters: Adjuster[];
   currentSettings?: {
     universal_filters?: Record<string, string[]>;
-    [key: string]: any;
+    [key: string]: unknown;
   };
   onLoadPipeline: (filters: Record<string, string[]>) => void;
   onPipelineChange?: (pipeline: Pipeline | null) => void;
