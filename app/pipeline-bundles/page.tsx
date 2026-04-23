5b53555f15190257f3bcf053a8e22a0ceab7806b
a5-001 <orvill.delatorre@atom5engineering.com>
2026-04-23 08:42:30 -0400

    -

--- app/pipeline-bundles/page.tsx
+++ app/pipeline-bundles/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useMemo, useState } from "react";
 import { PricingOverview } from "../pricing/components/pricing-overview";
 export default function PipelineBundlesPage() {
