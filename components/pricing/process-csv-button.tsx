e69c6203cfe62dacb36a3963ecf507ec0038e911
a5-001 <orvill.delatorre@atom5engineering.com>
2026-07-06 22:35:48 -0400

    -

--- components/pricing/process-csv-button.tsx
+++ components/pricing/process-csv-button.tsx
@@ -40,13 +40,13 @@ import type { CalculatedPriceRow } from "@/components/pipelines/calculated-price
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
